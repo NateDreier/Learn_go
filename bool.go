1562a4e26790664ee00e1f402f1a04bc3f692237
Nate Dreier <ndreier@maystreet.com>
2021-06-27 08:43:43 -0700

    init

---
+++ bool.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("bool:", 1 > 2)
+  fmt.Println("bool:", 1 < 2)
+  fmt.Println("bool:", 1 <= 2)
+  fmt.Println("bool:", 2 == 2)
+  fmt.Println("bool:", 1 != 2)
+  fmt.Println("bool:", 1 == 2)
+}
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import ( 
+      "fmt"
+      "math"
+)
+      
+/*
+shalom
+*/
+
+// hallo
+// here is some good docs for my helo func
+func main () {
+  fmt.Println("Addition:", 1 + 3)
+  fmt.Println("Subtraction:", 1 - 3)
+  fmt.Println("Multiplicaiton:", 1 * 3)
+  fmt.Println("Division:", 1.0/3)
+  fmt.Println("Exponent:", math.Pow(2.0, 3))
+}
