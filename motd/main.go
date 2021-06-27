5ca9f9d363ea074e47755b73c2f050d9d3091c34
Nate Dreier <ndreier@maystreet.com>
2021-06-27 12:42:22 -0700

    opening/closing/writing files

--- motd/main.go
+++ motd/main.go
@@ -3,10 +3,40 @@ package main
 import (
   "fmt"
   "motd/message"
+  "bufio"
+//  "io/ioutil"
+  "os"
+  "strings"
 )
 
 func main() {
-	m := message.Greeting("Nate", "hello")
-	fmt.Println(m)
+	f, err := os.OpenFile("/etc/motd", os.O_WRONLY, 0644)
+	
+	if err != nil {
+		fmt.Println("err err err")
+		os.Exit(1)
+	}
+
+	defer f.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Your Greeting: ")
+	phrase, _ := reader.ReadString('\n')
+	phrase = strings.TrimSpace(phrase)
+
+	fmt.Print("Your Name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	m := message.Greeting(name, phrase)
+	_, err = f.Write([]byte(m))
+
+	//err := ioutil.WriteFile("/etc/motd", []byte(m), 0644)
+
+	if err != nil {
+		fmt.Println("can't write!!")
+		os.Exit(1)
+	}
+
 }
 
